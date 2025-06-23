4bac36b0c31e257250e81ec9f4ab5320e069fbcf
Stefan Trajkovski <trajkovski.stefan@hotmail.com>
2025-06-23 13:53:30 +0200

    add vite and package.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/nokti-termini-makedonski/",  // <-- ADD THIS LINE for GitHub Pages
   server: {
     host: "::",
     port: 8080,
